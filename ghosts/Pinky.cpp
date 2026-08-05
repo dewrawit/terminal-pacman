fbca2be7b5e8cef240bdf005b02feb7c0714eb0d
Dew <dewrawit@gmail.com>
2026-08-05 16:42:26 +0700

    Rework Pinky targetting AI to make it match arcade version better

--- ghosts/Pinky.cpp
+++ ghosts/Pinky.cpp
@@ -15,28 +15,13 @@ void Pinky::setTarget(GameState& gameState)
     //Targets 4 tiles ahead
     constexpr int pinkyTargetRange { 4 };
 
-    const Board& board { gameState.getBoard() };
-    Position positionToCheck { gameState.getPacman().getPosition() };
-    
+    Position pacmanPosition { gameState.getPacman().getPosition() };
     Direction facingDirection { gameState.getPacman().getDirection() };
+
+    m_target = pacmanPosition;
 
     for(int step {0}; step < pinkyTargetRange; ++step)
     {
-        //Check if one tile ahead of current is a Wall or walkable
-        //There shouldn't be a dead end in the map (or loop never ends since we cant go backward)
-        while(true)
-        {
-            Position newPosition = positionToCheck + Entity::getDirectionOffset(facingDirection);
-
-            if(board.getTileAtPosition(newPosition).isWalkable())
-            {
-                positionToCheck = newPosition;
-                break;
-            }
-
-            //Change direction
-            facingDirection = Entity::getNonOppositeRandomDirection(facingDirection);
-        }
+        m_target = m_target + getDirectionOffset(facingDirection);
     }
-    m_target = positionToCheck; //4 tiles ahead of pacman position that is walkable
 }
